2a4bed4380a7f65325a1265a870d608e2b272c17
keep-steady <38748880+keep-steady@users.noreply.github.com>
2019-01-28 18:07:02 +0900

    Add files via upload

---
+++ simple_nmt/rl_trainer_custom.py
@@ -0,0 +1,214 @@
+from tqdm import tqdm
+# from nltk.translate.bleu_score import sentence_bleu as score_func
+from nltk.translate.gleu_score import sentence_gleu as score_func
+# from utils import score_sentence as score_func
+
+import torch
+import torch.nn.utils as torch_utils
+
+import utils
+import data_loader
+
+# from simple_nmt.trainer import Trainer
+from simple_nmt.trainer_custom import Trainer
+
+class MinimumRiskTrainer(Trainer):
+
+    def __init__(self, model, crit, config, **kwargs):
+        super().__init__(model=model, crit=crit, config=config, **kwargs)
+
+        self.n_epochs = config.n_epochs + config.rl_n_epochs
+        self.lower_is_better = False
+        self.best = {'epoch': config.n_epochs,
+                     'current_lr': config.rl_lr,
+                     'config': config,
+                     **kwargs
+                     }
+
+    def _get_reward(self, y_hat, y, n_gram=6):
+        # NLTK의 sentence_gleu 함수를 사용하여,y와 y_hat의 GLEU 값을 구해서 출력
+        # This method gets the reward based on the sampling result and reference sentence.
+        # For now, we uses GLEU in NLTK, but you can used your own well-defined reward function.
+        # In addition, GLEU is variation of BLEU, and it is more fit to reinforcement learning.
+
+        # Since we don't calculate reward score exactly as same as multi-bleu.perl,
+        # (especialy we do have different tokenization,) I recommend to set n_gram to 6.
+
+        # |y| = (batch_size, length1)
+        # |y_hat| = (batch_size, length2)
+
+        scores = []
+
+        # Actually, below is really far from parallized operations.
+        # Thus, it may cause slow training.
+        for b in range(y.size(0)):
+            ref = []
+            hyp = []
+            for t in range(y.size(1)):
+                ref += [str(int(y[b, t]))]
+                if y[b, t] == data_loader.EOS:
+                    break
+
+            for t in range(y_hat.size(1)):
+                hyp += [str(int(y_hat[b, t]))]
+                if y_hat[b, t] == data_loader.EOS:
+                    break
+
+            # for nltk.bleu & nltk.gleu
+            scores += [score_func([ref], hyp, max_len=n_gram) * 100.]
+
+            # for utils.score_sentence
+            # scores += [score_func(ref, hyp, 4, smooth = 1)[-1] * 100.]
+        scores = torch.FloatTensor(scores).to(y.device)
+        # |scores| = (batch_size)
+
+        return scores
+
+    def _get_gradient(self, y_hat, y, crit=None, reward=1):
+        # |y| = (batch_size, length)
+        # |y_hat| = (batch_size, length, output_size)
+        # |reward| = (batch_size)
+        crit = self.crit if crit is None else crit
+
+        # Before we get the gradient, multiply -reward for each sample and each time-step.
+        y_hat = y_hat * -reward.view(-1, 1, 1).expand(*y_hat.size())
+
+        # Again, multiply -1 because criterion is NLLLoss.
+        log_prob = -crit(y_hat.contiguous().view(-1, y_hat.size(-1)),
+                         y.contiguous().view(-1)
+                         )
+        log_prob.div(y.size(0)).backward()
+
+        return log_prob
+
+    def train_epoch(self,
+                    train,
+                    optimizer,
+                    max_grad_norm=5
+                   ):
+        '''
+        Train an epoch with given train iterator and optimizer.
+        '''
+        total_reward, total_actor_reward = 0, 0
+        total_grad_norm = 0
+        avg_reward, avg_actor_reward = 0, 0
+        avg_param_norm, avg_grad_norm = 0, 0
+        sample_cnt = 0
+
+        # Iterate whole train-set.
+        for idx, mini_batch in enumerate(train):
+            # Raw target variable has both BOS and EOS token. 
+            # The output of sequence-to-sequence does not have BOS token. 
+            # Thus, remove BOS token for reference.
+            x, y = mini_batch.src, mini_batch.tgt[0][:, 1:]
+            # |x| = (batch_size, length)
+            # |y| = (batch_size, length)
+            
+            # You have to reset the gradients of all model parameters before to take another step in gradient descent.
+            optimizer.zero_grad()
+
+            # Take sampling process because set False for is_greedy.
+            y_hat, indice = self.model.search(x,
+                                              is_greedy=False,
+                                              max_length=self.config.max_length
+                                              )
+            # Based on the result of sampling, get reward.
+            actor_reward = self._get_reward(indice,
+                                            y,
+                                            n_gram=self.config.rl_n_gram
+                                            )
+            # |y_hat| = (batch_size, length, output_size)
+            # |indice| = (batch_size, length)
+            # |actor_reward| = (batch_size)
+
+            # Take samples as many as n_samples, and get average rewards for them.
+            # I figured out that n_samples = 1 would be enough.
+            baseline = []
+            with torch.no_grad():
+                for i in range(self.config.n_samples):
+                    _, sampled_indice = self.model.search(x,
+                                                          is_greedy=False,
+                                                          max_length=self.config.max_length
+                                                          )
+                    baseline += [self._get_reward(sampled_indice,
+                                                  y,
+                                                  n_gram=self.config.rl_n_gram
+                                                  )]
+                baseline = torch.stack(baseline).sum(dim=0).div(self.config.n_samples)
+                # |baseline| = (n_samples, batch_size) --> (batch_size)
+
+            # Now, we have relatively expected cumulative reward.
+            # Which score can be drawn from actor_reward subtracted by baseline.
+            final_reward = actor_reward - baseline
+            # |final_reward| = (batch_size)
+
+            # calcuate gradients with back-propagation
+            self._get_gradient(y_hat, indice, reward=final_reward)
+            
+            # Simple math to show stats.
+            total_reward += float(final_reward.sum())
+            total_actor_reward += float(actor_reward.sum())
+            sample_cnt += int(actor_reward.size(0))
+            total_grad_norm += float(utils.get_grad_norm(self.model.parameters()))
+
+            avg_reward = total_reward / sample_cnt
+            avg_actor_reward = total_actor_reward / sample_cnt
+            avg_grad_norm = total_grad_norm / (idx + 1)
+
+            # In orther to avoid gradient exploding, we apply gradient clipping.
+            torch_utils.clip_grad_norm_(self.model.parameters(),
+                                        self.config.max_grad_norm
+                                        )
+            # Take a step of gradient descent.
+            optimizer.step()
+
+            if idx >= len(train) * self.config.train_ratio_per_epoch:
+                break
+        
+#         print('Train_BLEU=%.4f' % avg_actor_reward)
+        
+        return avg_actor_reward, avg_param_norm, avg_grad_norm
+
+    def validate(self,
+                 valid,
+                 crit=None
+                 ):
+        '''
+        Validate a model with given valid iterator.
+        '''
+        # We don't need to back-prop for these operations.
+        with torch.no_grad():
+            total_reward, sample_cnt = 0, 0
+
+            self.model.eval()
+            # Iterate for whole valid-set.
+            for idx, mini_batch in enumerate(valid):
+                x, y = mini_batch.src, mini_batch.tgt[0][:, 1:]
+                # |x| = (batch_size, length)
+                # |y| = (batch_size, length)
+
+                # feed-forward
+                y_hat, indice = self.model.search(x,
+                                                  is_greedy=True,
+                                                  max_length=self.config.max_length
+                                                  )
+                # |y_hat| = (batch_size, length, output_size)
+                # |indice| = (batch_size, length)
+                reward = self._get_reward(indice,
+                                          y,
+                                          n_gram=self.config.rl_n_gram
+                                          )
+
+                total_reward += float(reward.sum())
+                sample_cnt += int(mini_batch.tgt[0].size(0))
+                
+                avg_reward = total_reward / sample_cnt
+
+                if idx >= len(valid):
+                    break
+
+            self.model.train()
+
+#             print('valid_BLEU=%.4f' % avg_reward)
+            
+            return avg_reward
---
+++ simple_nmt/trainer_custom.py
@@ -0,0 +1,216 @@
+from tqdm import tqdm
+from math import exp
+import datetime
+
+import torch
+import torch.optim as optim
+import torch.nn.utils as torch_utils
+
+import utils
+
+class Trainer():
+    def __init__(self, model, crit, config, **kwargs):
+        self.model = model
+        self.crit = crit
+        self.config = config
+
+        super().__init__()
+
+        self.n_epochs = config.n_epochs
+        self.lower_is_better = True
+        self.best = {'epoch': 0,
+                     'current_lr': config.lr,
+                     'config': config,
+                     **kwargs
+                     }
+
+    def get_best_model(self):
+        self.model.load_state_dict(self.best['model'])
+
+        return self.model
+
+    def save_training(self, fn):
+        torch.save(self.best, fn)
+
+    def _get_loss(self, y_hat, y, crit=None):
+        # |y_hat| = (batch_size, length, output_size)
+        # |y| = (batch_size, length)
+        crit = self.crit if crit is None else crit
+        loss = crit(y_hat.contiguous().view(-1, y_hat.size(-1)),
+                    y.contiguous().view(-1)
+                    )
+
+        return loss
+
+    def train_epoch(self,
+                    train,
+                    optimizer
+                    ):
+        '''
+        Train an epoch with given train iterator and optimizer.
+        '''
+        total_loss, total_word_count = 0, 0
+        total_grad_norm = 0
+        avg_loss, avg_grad_norm = 0, 0
+        sample_cnt = 0
+
+        # Iterate whole train-set.
+        for idx, mini_batch in enumerate(train):
+            # Raw target variable has both BOS and EOS token. 
+            # The output of sequence-to-sequence does not have BOS token. 
+            # Thus, remove BOS token for reference.
+            x, y = mini_batch.src, mini_batch.tgt[0][:, 1:]
+            # |x| = (batch_size, length)
+            # |y| = (batch_size, length)
+
+            # You have to reset the gradients of all model parameters before to take another step in gradient descent.
+            optimizer.zero_grad()
+
+            # Take feed-forward
+            # Similar as before, the input of decoder does not have EOS token.
+            # Thus, remove EOS token for decoder input.
+            y_hat = self.model(x, mini_batch.tgt[0][:, :-1])
+            # |y_hat| = (batch_size, length, output_size)
+
+            # Calcuate loss and gradients with back-propagation.
+            loss = self._get_loss(y_hat, y)
+            loss.div(y.size(0)).backward()
+
+            # Simple math to show stats.
+            # Don't forget to detach final variables.
+            total_loss += float(loss)
+            total_word_count += int(mini_batch.tgt[1].sum())
+            param_norm = float(utils.get_parameter_norm(self.model.parameters()))
+            total_grad_norm += float(utils.get_grad_norm(self.model.parameters()))
+
+            avg_loss = total_loss / total_word_count
+            avg_grad_norm = total_grad_norm / (idx + 1)
+
+            # In orther to avoid gradient exploding, we apply gradient clipping.
+            torch_utils.clip_grad_norm_(self.model.parameters(),
+                                        self.config.max_grad_norm
+                                        )
+            # Take a step of gradient descent.
+            optimizer.step()
+
+            sample_cnt += mini_batch.tgt[0].size(0)
+
+            if idx >= len(train) * self.config.train_ratio_per_epoch:
+                break
+
+        return avg_loss, param_norm, avg_grad_norm
+
+    def train(self, train, valid):
+        current_lr = self.best['current_lr']
+        best_loss = float('Inf') * (1 if self.lower_is_better else -1)
+        lowest_after = 0
+        train_loss_list = []
+        valid_loss_list = []
+        
+        start_time = datetime.datetime.now()
+        print('train start - epoch : %d / %s' % (self.n_epochs, start_time))
+        for idx in range(self.best['epoch'], self.n_epochs):  # Iterate from 1 to n_epochs
+            if self.config.adam:
+                optimizer = optim.Adam(self.model.parameters(), lr=current_lr)
+            else:
+                optimizer = optim.SGD(self.model.parameters(), lr=current_lr)
+
+            # train_epoch
+            avg_train_loss, avg_param_norm, avg_grad_norm = self.train_epoch( train, optimizer )
+            # valid_epoch
+            avg_valid_loss = self.validate(valid)
+            # loss list save
+            train_loss_list.append(avg_train_loss)
+            valid_loss_list.append(avg_valid_loss)
+            print('epoch: %d/%d\tloss_tran / valid:%.4e / %.4e\tcurrent_lr:%f' % (idx + 1,
+                                                                                 self.n_epochs,
+                                                                                 avg_train_loss,
+                                                                                 avg_valid_loss,
+                                                                                 current_lr
+                                                                                 ))
+
+            ## !! loss가 개선된 경우 best 업데이트
+            if (self.lower_is_better and avg_valid_loss < best_loss) or (not self.lower_is_better and avg_valid_loss > best_loss):
+                    # Update if there is an improvement.
+                best_loss = avg_valid_loss
+                lowest_after = 0
+
+                self.best['model'] = self.model.state_dict()
+                self.best['optim'] = optimizer
+                self.best['epoch'] = idx + 1
+                self.best['current_lr'] = current_lr
+
+                # Set a filename for model of last epoch.
+                # We need to put every information to filename, as much as possible.
+                model_fn = self.config.model.split('.')
+                ## general training(n_epochs 보다 idx가 작을 경우)
+                if self.config.n_epochs >= idx + 1:
+                    model_fn = model_fn[:-1] + ['%02d_' % (idx + 1),
+                                                'trainloss:%.2f_'  % (avg_train_loss),
+                                                'validloss:%.2f'   % (avg_valid_loss)
+                                                ] + [model_fn[-1]]
+                ## reinforcement training
+                else:
+                    model_fn = model_fn[:-1] + ['%02d' % (idx + 1),
+                                                'trainloss:%.2f'   % (avg_train_loss),
+                                                'validloss:%.2f_RL'% (avg_valid_loss)
+                                                ] + [model_fn[-1]]
+
+                ## model save
+                self.save_training('.'.join(model_fn))
+            ## !! early stop 이 켜져있고, config.early_stop보다 오래 진전이 없으면 스탑
+            else:
+                lowest_after += 1
+                if (lowest_after >= self.config.early_stop) and (self.config.early_stop > 0):
+                    print('Early Stop')
+                    break
+
+            # loss 향상이 없거나, 향상이 있어도 lr decay 시작 상수(10, config.lr_decay_start_at) 이상일 때 learning rate decay 적용(*0.5)
+            # Altough there is an improvement in last epoch, we need to decay the learning-rate if it meets the requirements.
+            if ((lowest_after > 0) or (idx + 1 >= self.config.lr_decay_start_at)) and (idx + 1 <= self.config.n_epochs):
+                current_lr = max(self.config.min_lr,
+                                 current_lr * self.config.lr_decay_rate
+                                 )
+        # train, val losee return
+        print(datetime.datetime.now() - start_time)  # 수행시간 측정
+        return train_loss_list, valid_loss_list
+        
+    def validate(self,
+                 valid,
+                 crit=None
+                ):
+        '''
+        Validate a model with given valid iterator.
+        '''
+        # We don't need to back-prop for these operations.
+        with torch.no_grad():
+            sample_cnt = 0
+            total_loss, total_word_count = 0, 0
+
+            self.model.eval()
+            # Iterate for whole valid-set.
+            for idx, mini_batch in enumerate(valid):
+                x, y = mini_batch.src, mini_batch.tgt[0][:, 1:]
+                # |x| = (batch_size, length)
+                # |y| = (batch_size, length)
+                y_hat = self.model(x, mini_batch.tgt[0][:, :-1])
+                # |y_hat| = (batch_size, n_classes)
+
+                loss = self._get_loss(y_hat, y, crit)
+
+                total_loss += float(loss)
+                total_word_count += int(mini_batch.tgt[1].sum())
+                avg_loss = total_loss / total_word_count
+
+                sample_cnt += mini_batch.tgt[0].size(0)
+
+                if idx >= len(valid):
+                    break
+
+            self.model.train()
+
+            # loss가 너무 큰경우, exp 에러남, exp(700) 까지만 계산 가능
+            if avg_loss > 700:avg_loss = 700
+#             print('valid_loss=%.4e PPL=%.2f' % (avg_loss, exp(avg_loss)))
+                
+            return avg_loss
